e89b8b83749f744ffbb2af47c3256c2511de2491
Safak Ciplak <safakciplak1990@gmail.com>
2022-07-05 17:52:14 +0300

    loggin with logrus

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/rest-api/internal/comment"
 	"github.com/rest-api/internal/database"
 	transportHTTP "github.com/rest-api/internal/transport/http"
+	log "github.com/sirupsen/logrus"
 )
 
-// App - the struct which contains things like pointers
-// to database connections
-type App struct{}
+// App - contain application information
+type App struct {
+	Name    string
+	Version string
+}
 
 // Run - sets up our application
 func (app *App) Run() error {
-	fmt.Println("Setting up our application")
+	log.SetFormatter(&log.JSONFormatter{})
+	log.WithFields(
+		log.Fields{
+			"AppName":    app.Name,
+			"AppVersion": app.Version,
+		}).Info("Setting up our application")
 
 	var err error
 	db, err := database.NewDatabase()
@@ -33,7 +40,7 @@ func (app *App) Run() error {
 	handler.SetupRoutes()
 
 	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
-		fmt.Println("Failed to set up server")
+		log.Error("Failed to set up server")
 		return err
 	}
 
@@ -41,11 +48,12 @@ func (app *App) Run() error {
 }
 
 func main() {
-	fmt.Println("Go REST API")
-
-	app := App{}
+	app := App{
+		Name:    "Commenting Service",
+		Version: "1.0.0",
+	}
 	if err := app.Run(); err != nil {
-		fmt.Println("Error starting up our REST API")
-		fmt.Println(err)
+		log.Error("Error starting up our REST API")
+		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -10,8 +10,10 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/lib/pq v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
